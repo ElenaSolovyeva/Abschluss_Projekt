741994e943ec2bae0e456d6f4337983cee877e1f
Elena Solovyeva <elena.solo.mail2014@gmail.com>
2023-11-30 12:46:22 +0100

    Add changes 30.11.2023 12:45

---
+++ MaterialStatus/Controllers/AdminAreaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialStatus.Controllers
+{
+    public class AdminAreaController : Controller
+    {
+        //public IActionResult AdminArea()
+        //{
+        //    return View(@"Views\Home\AdminArea.cshtml");
+        //    return View();
+        //}
+    }
+}
