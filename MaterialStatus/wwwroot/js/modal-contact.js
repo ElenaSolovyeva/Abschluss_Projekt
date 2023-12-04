0b545a6560249a24427b2ab055acfa875092d942
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2023-12-04 08:47:47 +0100

    Add js for open/close the modal window with contacts

---
+++ MaterialStatus/wwwroot/js/modal-contact.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+const modalContact = document.getElementById('modal-contact');
+const btnOpenContact = document.getElementById('contact-open');
+const btnCloseContact = document.getElementById('contact-close');
+
+btnOpenContact.onclick = function () {
+	modalContact.style.display = "flex";
+}
+
+btnCloseContact.onclick = function () {
+	modalContact.style.display = "none";
+}
