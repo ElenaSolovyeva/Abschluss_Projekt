fc75e509a9ced6c78a6bb4bcf354754231616c1c
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2023-12-05 16:26:22 +0100

    Add contacts to html from config-file

--- MaterialStatus/Service/Config.cs
+++ MaterialStatus/Service/Config.cs
@@ -11,9 +11,16 @@ namespace MaterialStatus.Service
         public static string Version { get; set; } 
         public static string ConnectionString_B { get; set; }
         public static string ConnectionString_G { get; set; }
-        //public static string CompanyName { get; set; }
-        //public static string CompanyPhone { get; set; }
-        //public static string CompanyPhoneShort { get; set; }
-        //public static string CompanyEmail { get; set; }
+        public static string CompanyName { get; set; }
+        public static string CompanyPhone { get; set; }
+        public static string CompanyPhoneShort { get; set; }
+        public static string CompanyFax { get; set; }
+        public static string CompanyFaxShort { get; set; }
+        public static string CompanyEmail { get; set; }
+        public static string CompanyStreet { get; set; }
+        public static string CompanyCity { get; set; }
+        public static string CompanyWebSite { get; set; }
+        public static string CompanyWebSiteShort { get; set; }
+        public static string CompanyGoogleMapLink { get; set; }
     }
 }
