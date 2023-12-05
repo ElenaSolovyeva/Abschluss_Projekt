5ac31c579bc5258837fdb96126346e75799e26ab
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2023-12-05 15:43:05 +0100

    Add connection string in Config.cs and appsettigs.json

--- MaterialStatus/Service/Config.cs
+++ MaterialStatus/Service/Config.cs
@@ -9,10 +9,11 @@ namespace MaterialStatus.Service
     public class Config
     {
         public static string Version { get; set; } 
-        public static string ConnectionString { get; set; }
-        public static string CompanyName { get; set; }
-        public static string CompanyPhone { get; set; }
-        public static string CompanyPhoneShort { get; set; }
-        public static string CompanyEmail { get; set; }
+        public static string ConnectionString_B { get; set; }
+        public static string ConnectionString_G { get; set; }
+        //public static string CompanyName { get; set; }
+        //public static string CompanyPhone { get; set; }
+        //public static string CompanyPhoneShort { get; set; }
+        //public static string CompanyEmail { get; set; }
     }
 }
