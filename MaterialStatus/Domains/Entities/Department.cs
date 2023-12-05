3e411e917f67a685761873527d48ed19fbd042a4
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2023-12-05 16:52:54 +0100

    Add entities User, UsersGroup and Department in the domain model

---
+++ MaterialStatus/Domains/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaterialStatus.Domains.Entities
+{
+    public class Department
+    {
+        protected Department()
+        {
+            DateAdded = DateTime.UtcNow;
+        }
+
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Benennung von der Abteilung")]
+        public virtual string Name { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ MaterialStatus/Domains/Entities/UsersGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaterialStatus.Domains.Entities
+{
+    public class UsersGroup
+    {
+        protected UsersGroup()
+        {
+            DateAdded = DateTime.UtcNow;
+        }
+
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Benennung von der Benutzergruppe")]
+        public virtual string Name { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
+
+
