cb9a2238140a955bc07356a4f6caa96e4829222b
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2024-01-26 12:10:36 +0100

    Make MainrPage on IndexPage and change HomeController

--- MaterialStatus/Controllers/HomeController.cs
+++ MaterialStatus/Controllers/HomeController.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using MaterialStatus.Features.SWB.Models;
+using MaterialStatus.Features.SWG.Models;
+using System;
+using System.Collections.Generic;
 
 namespace MaterialStatus.Controllers
 {
     public class HomeController : Controller        
-    {       
-        public IActionResult Index()
+    {
+        private readonly SwbDbContext contextSWB;
+        private readonly SwgDbContext contextSWG;
+
+        public HomeController(SwbDbContext contextSWB, SwgDbContext contextSWG)
         {
+            this.contextSWB = contextSWB;
+            this.contextSWG = contextSWG;
+        }
+
+        public IActionResult MainPage()
+        {
+            ViewData["DispoB"] = contextSWB.tbl_dispo.ToList();
+            ViewData["FarbeBezeichnungB"] = contextSWB.tbl_stueckliste_historie_kopf.ToList();
+            ViewData["DispoG"] = contextSWG.tbl_dispo.ToList();
+            ViewData["LagerG"] = contextSWG.tbl_lager.ToList();
+            ViewData["MaterialG"] = contextSWG.tbl_material.ToList();
+            ViewData["EquipementDosierG"] = contextSWG.tbl_equipement_dosier.ToList();
+
+
+            List<String?> rohstoffSilos = new List<string?>();
+            foreach (var eqDosierG in ViewData["EquipementDosierG"]
+                        as List<MaterialStatus.Features.SWG.Models.tbl_equipement_dosier>)
+            {
+                foreach (var lagerG in ViewData["LagerG"]
+                            as List<MaterialStatus.Features.SWG.Models.tbl_lager>)
+                {
+                    if (eqDosierG.lager_Id == lagerG.id
+                        && lagerG.aktiv == 1
+                        && lagerG.aktiv1 == 1)
+
+                        rohstoffSilos.Add(lagerG.sap_bezeichnung);
+                }
+            }
+
+            rohstoffSilos.Sort();
+
+            ViewData["RohstoffSilos"] = rohstoffSilos;
+
             return View();
         }
     }
